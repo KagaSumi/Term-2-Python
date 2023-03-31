290ab4c3685b9b93f9da97f7283aa1c9f8a47020
Quinten Leung <wleung105@my.bcit.ca>
2023-03-30 17:49:25 -0700

    Just sorting by time left

--- Final Project/GUI/main.py
+++ Final Project/GUI/main.py
@@ -29,15 +29,15 @@ class Main(qtw.QMainWindow, Ui_Main):
         self.rb_order_no_filter.clicked.connect(self.order_no_filter)
         self.rb_order_unprocessed.clicked.connect(self.order_unprocessed_filter)
         self.rb_order_processed.clicked.connect(self.order_processed_filter)
+        self.le_order_filter.textChanged.connect(self.update_order_table)
         self.fetch_product_table()
         self.fetch_order_table()
-
 
     def fetch_product_table(self):
         response = requests.get(URL + "/product")
         content = json.loads(response.text)
         if self.cb_out_of_stock.isChecked():
-            content = [item for item in content if item["quantity"] != 0]
+            content = [item for item in content if item["quantity"] == 0]
         table = self.tv_product_list
         model = qtg.QStandardItemModel()
         model.setColumnCount(3)
@@ -75,17 +75,19 @@ class Main(qtw.QMainWindow, Ui_Main):
             content = [order for order in content if order["completed"]]
         elif self.order_process_sort == False:
             content = [order for order in content if not order["completed"]]
+        if self.le_order_filter.text():
+            content = [order for order in content if self.le_order_filter.text().lower() in order["customer_name"].lower()]
         model = qtg.QStandardItemModel()
         model.setColumnCount(5)
         model.setHorizontalHeaderLabels(['Order_id', 'Name', 'Address','Time Created', 'Time Processed'])
         for row_num, row_data in enumerate(content):
-            id = qtg.QStandardItem(str(row_data['id']))
+            order_id = qtg.QStandardItem(str(row_data['id']))
             name = qtg.QStandardItem(row_data['customer_name'].title())
             address = qtg.QStandardItem(row_data['customer_address'].title())
             time_create = qtg.QStandardItem(row_data['time_created'])
             time_process = qtg.QStandardItem(row_data['time_processed'])
 
-            model.setItem(row_num, 0, id)
+            model.setItem(row_num, 0, order_id)
             model.setItem(row_num, 1, name)
             model.setItem(row_num, 2, address)
             model.setItem(row_num, 3, time_create)
@@ -133,6 +135,7 @@ class Main(qtw.QMainWindow, Ui_Main):
     def update_order_table(self):
         self.order_list = json.loads(requests.get(URL + "/order").text)
         self.fetch_order_table()
+
 # ----------------------------------------------------------------
 # ----------------- Button Functionality--------------------------
 # ----------------------------------------------------------------
@@ -169,6 +172,7 @@ class Main(qtw.QMainWindow, Ui_Main):
     def process_update_order(self):
         self.Order_Update_Form = Order_Edit_Form(self.selected_order_id)
         self.Order_Update_Form.finished.connect(self.update_order_table)
+        self.tv_order_items.setModel(None)
         self.Order_Update_Form.show()
 
     @qtc.Slot()
